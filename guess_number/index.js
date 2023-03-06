efabc8a535e1088c18a6c7f27f0ad8dc3392a7cd
Farrukh <farrukh007@gmail.com>
2023-03-07 01:05:36 +0500

    Added new project Guess Number

---
+++ guess_number/index.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+async function welcome() {
+    let rainbowTitle = chalkAnimation.rainbow("Lets Guess a Number Between 1 to 10!"); // start animation here
+    //let rainbowTitle = chalkAnimation.glitch("Lets Start Calculation"); // start animation here
+    await sleep();
+    rainbowTitle.stop();
+}
+await welcome();
+const answer = await inquirer.prompt([
+    {
+        name: "guessNum",
+        type: "number",
+        message: "Enter Number: "
+    }
+]);
+console.log(answer);
